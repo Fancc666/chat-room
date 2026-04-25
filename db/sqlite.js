54e43d332f4b70e02d027d23a35eb1d2c7796b5b
Fancc666 <fcc565455@163.com>
2026-04-25 14:36:48 +0800

    ``` feat(db): 使用 better-sqlite3 替代 sqlite3
    - 将数据库驱动从 sqlite3 更换为 better-sqlite3
    - 修改数据库连接方式以适配新的驱动
    - 将表创建方法从 run 改为 exec
    - 重构 getRecord 函数使用 prepare 语句提高性能
    - 重构 setRecord 函数使用 prepare 语句提高性能
    - 移除 Promise 包装，直接返回查询结果
    ```

--- db/sqlite.js
+++ db/sqlite.js
@@ -1,11 +1,11 @@
 'use strict'
 
 const path = require('path')
-const sqlite3 = require('sqlite3')
+const Database = require('better-sqlite3')
 
-const db = new sqlite3.Database(path.resolve(__dirname, '../msg.db'))
+const db = new Database(path.resolve(__dirname, '../msg.db'))
 
-db.run(`CREATE TABLE IF NOT EXISTS tb_msg (
+db.exec(`CREATE TABLE IF NOT EXISTS tb_msg (
     id        INTEGER        PRIMARY KEY AUTOINCREMENT
                              NOT NULL
                              UNIQUE,
@@ -19,36 +19,24 @@ db.run(`CREATE TABLE IF NOT EXISTS tb_msg (
     msg       VARCHAR (1000) NOT NULL
 );`)
 
-function getRecord(roomId, limit=100, offset=0) {
-  return new Promise((resolve, reject) => {
-    db.all('SELECT * from tb_msg WHERE `room` = ? ORDER BY `time` DESC LIMIT ? OFFSET ?', roomId, limit, offset, (err, row) => {
-      if (err) reject(err)
-
-      resolve(row)
-    })
-  })
+function getRecord(roomId, limit = 100, offset = 0) {
+  const stmt = db.prepare('SELECT * from tb_msg WHERE `room` = ? ORDER BY `time` DESC LIMIT ? OFFSET ?')
+  return stmt.all(roomId, limit, offset)
 }
 
 function setRecord(msgItem) {
   const { name, room, uid, sid, ts: time, namecolor, msgcolor, msg } = msgItem
-  return new Promise((resolve, reject) => {
-    db.run(`INSERT INTO tb_msg(\`name\`, \`room\`, \`uid\`, \`sid\`, \`time\`, \`namecolor\`, \`msgcolor\`, \`msg\`)
-            VALUES($name, $room, $uid, $sid, $time, $namecolor, $msgcolor, $msg);`
-      , {
-        $name: name,
-        $room: room,
-        $uid: uid,
-        $sid: sid,
-        $time: time,
-        $namecolor: namecolor,
-        $msgcolor: msgcolor,
-        $msg: msg
-      }
-      , (err, row) => {
-        if (err) reject(err)
-
-        resolve(row)
-      })
+  const stmt = db.prepare(`INSERT INTO tb_msg(\`name\`, \`room\`, \`uid\`, \`sid\`, \`time\`, \`namecolor\`, \`msgcolor\`, \`msg\`)
+            VALUES($name, $room, $uid, $sid, $time, $namecolor, $msgcolor, $msg);`)
+  return stmt.run({
+    $name: name,
+    $room: room,
+    $uid: uid,
+    $sid: sid,
+    $time: time,
+    $namecolor: namecolor,
+    $msgcolor: msgcolor,
+    $msg: msg
   })
 }
 
